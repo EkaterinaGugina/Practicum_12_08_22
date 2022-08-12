005163dc92b636538a85dc77f71ab0a17f949356
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-12 22:39:26 +0500

    rename Ex001

---
+++ Ex001_Decart_kvart/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает два числа и проверяет, является ли одно квадратом другого.
+Console.WriteLine("Введите два целых числа, и мы выведем, является ли одно из них квадратом другого");
+Console.Write("a =");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("b =");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+    {
+     Console.WriteLine($"{numberA} квадрат {numberB}");
+    }
+else if (numberB == numberA * numberA)
+    {
+     Console.WriteLine($"{numberB} квадрат {numberA}");
+    }
+else
+    {
+     Console.WriteLine("Среди этих чисел нет квадрата другого");
+    }
