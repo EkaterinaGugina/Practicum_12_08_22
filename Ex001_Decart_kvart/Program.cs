faa5ca9c8e1ec507c0d7732d4fd26e4341d54856
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-13 00:37:00 +0500

    Ex001 solved

--- Ex001_Decart_kvart/Program.cs
+++ Ex001_Decart_kvart/Program.cs
@@ -7,25 +7,21 @@ int y_a = Convert.ToInt32(Console.ReadLine());
 
 if (x_a == 0 || y_a == 0)
 {
-    Console.WriteLine("Точка лежит на осях, не в четверти!")
+    Console.WriteLine("Точка лежит на осях, не в четверти!");
 }
-if (x_a > 0 )
-     if (y_a > 0)
+if (x_a > 0 && y_a > 0)
      {
-        Console.WritLine("1 четверть")
+        Console.WritLine("1 четверть");
      }
-     else if (y_a < 0)
+if (x_a > 0 && y_a < 0)
      {
-        Console.WritLine("4 четверть")
+        Console.WritLine("4 четверть");
+     }  
+if (x_a < 0 && y_a > 0)
+     {
+        Console.WritLine("2 четверть";
      }
-     
-     Console.WriteLine($"{numberA} квадрат {numberB}");
-    }
-else if (numberB == numberA * numberA)
-    {
-     Console.WriteLine($"{numberB} квадрат {numberA}");
-    }
-else
-    {
-     Console.WriteLine("Среди этих чисел нет квадрата другого");
-    }
+if (x_a < 0 && y_a < 0)
+     {
+        Console.WritLine("3 четверть");
+     }
