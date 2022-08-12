9e5f9efc09766a5a912a0c462dcb91661f63b6b1
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-13 00:31:00 +0500

    created solution for Ex001

--- Ex001_Decart_kvart/Program.cs
+++ Ex001_Decart_kvart/Program.cs
@@ -1,11 +1,24 @@
-﻿// Программа, которая на вход принимает два числа и проверяет, является ли одно квадратом другого.
-Console.WriteLine("Введите два целых числа, и мы выведем, является ли одно из них квадратом другого");
-Console.Write("a =");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("b =");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberA == numberB * numberB)
-    {
+﻿// Программа, для двух введенных с консоли чисел (координат точки) выводит в какой координатной четверти она лежит.
+Console.WriteLine("Введите координаты точек");
+Console.Write("x_a =");
+int x_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("y_a =");
+int y_a = Convert.ToInt32(Console.ReadLine());
+
+if (x_a == 0 || y_a == 0)
+{
+    Console.WriteLine("Точка лежит на осях, не в четверти!")
+}
+if (x_a > 0 )
+     if (y_a > 0)
+     {
+        Console.WritLine("1 четверть")
+     }
+     else if (y_a < 0)
+     {
+        Console.WritLine("4 четверть")
+     }
+     
      Console.WriteLine($"{numberA} квадрат {numberB}");
     }
 else if (numberB == numberA * numberA)
