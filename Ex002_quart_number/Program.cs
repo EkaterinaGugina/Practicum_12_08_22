104e1a946d0095b8e57701c630739bad6d75f117
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-13 01:02:19 +0500

    update folder Practica

---
+++ Ex002_quart_number/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход число  - номер координатной четверти и выдаёт диапазон значений для координат точек этой четверти
+Console.Write("Введите номер координатной четверти, number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <0 || number > 4)
+{
+    Console.WriteLine("Это не номер четверти!");
+}
+if (number == 1)
+     {
+        Console.WriteLine("x > 0, y > 0");
+     } 
+if (number == 4)
+     {
+        Console.WriteLine("x > 0, y < 0");
+     }  
+if (number == 2)
+     {
+        Console.WriteLine("x < 0, y > 0");
+     }
+if (number == 3)
+     {
+        Console.WriteLine("x < 0,  y < 0");
+     }
